6e46a18d8305c3d7733e795c9b1e206106cb99cb
tikdik <tikdik1988@gmail.com>
2017-12-06 21:17:56 +0800

    add send and receive

--- src/main/java/com/tikdik/App.java
+++ src/main/java/com/tikdik/App.java
@@ -14,14 +14,30 @@ public class App
     public static void main( String[] args ) {
 
         MessageDispatch dispatch = new MessageDispatch();
+        //发第一条消息
         Object message = new FirstMessage("zw", 30);
-        MessageTag tag = message.getClass().getAnnotation(MessageTag.class);
+        MessageTag msgTypeSend = message.getClass().getAnnotation(MessageTag.class);
+        String msgBodySend = JSON.toJSONString(message);
+        String msgSend = msgTypeSend.value() + ":" + msgBodySend;
 
-        dispatch.dispatch(tag.value(), JSON.toJSONString(message));
+        //接收第一条消息
+        int split = msgSend.indexOf(":");
+        String msgType = msgSend.substring(0, split);
+        String msgBody = msgSend.substring(split + 1);
 
+        dispatch.dispatch(msgType, msgBody);
+
+        //发第二条消息
         message = new SecondMessage("ha", 20);
-        tag = message.getClass().getAnnotation(MessageTag.class);
+        msgTypeSend = message.getClass().getAnnotation(MessageTag.class);
+        msgBodySend = JSON.toJSONString(message);
+        msgSend = msgTypeSend.value() + ":" + msgBodySend;
 
-        dispatch.dispatch(tag.value(), JSON.toJSONString(message));
+        //接收第二条
+        split = msgSend.indexOf(":");
+        msgType = msgSend.substring(0, split);
+        msgBody = msgSend.substring(split + 1);
+
+        dispatch.dispatch(msgType, msgBody);
     }
 }
